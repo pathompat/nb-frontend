01207e0f0c12582817ca5cefb24a8fb80aa77232
pathompat <lw93430@linecorp.com>
2024-11-19 23:36:05 +0700

    add store user and fetch api

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <h3>Hello world</h3>
-  </div>
+  <v-card>
+    <template v-slot:title>
+      <h3>Hello world</h3>
+    </template>
+
+    <v-card-text class="pt-4">
+      <div v-if="!users || users.length === 0">No data</div>
+      <div v-else>
+        <div v-for="user in users" :key="user.id">
+          {{ user.id }} {{ user.username }} {{ user.store }} {{ user.createdAt }} 
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
+
+<script setup lang="ts">
+  import { useUserStore } from '~/stores/user';
+  
+  // ref
+  const store = useUserStore();
+  const { users } = storeToRefs(store);
+
+  onMounted(() => {
+    store.fetchUsers()
+  })
+
+</script>
---
+++ stores/user.ts
@@ -0,0 +1,26 @@
+export const useUserStore = defineStore('user', () => {
+
+  // store
+  const users = ref<UserInfo[]>([])
+
+  // actions
+  const fetchUsers = async () => {
+    const { data }: any = await useFetch('/api/users');
+    if (data.value) {
+      users.value = data.value
+    }
+  }
+
+  return {
+    users,
+    fetchUsers,
+  };
+})
+
+interface UserInfo {
+  id: number
+  username: string
+  store: string
+  createdAt: string
+  updatedAt: string
+}
