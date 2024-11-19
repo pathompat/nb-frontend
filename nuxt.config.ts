67018ebea0341f44e512616b5f0c2816fbe44cc9
pathompat <lw93430@linecorp.com>
2024-11-19 23:35:29 +0700

    add env and pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,22 @@ export default defineNuxtConfig({
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module'],
+  modules: ['vuetify-nuxt-module', '@pinia/nuxt'],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
   vuetify: {
     moduleOptions: {
       /* module specific options */
     },
-    vuetifyOptions: './vuetify.config.ts' // <== you can omit it
-  }
+    vuetifyOptions: './vuetify.config.ts'
+  },
+  nitro: {
+    devProxy: {
+        "/api": {
+            target: process.env.API_URL,
+            changeOrigin: true,
+        }
+    }
+},
 })
