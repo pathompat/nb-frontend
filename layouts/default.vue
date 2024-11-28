566a4d4717cd17aba20b821ea9302bb55aa98382
atippaz <nasakun13201@gmail.com>
2024-11-28 19:55:24 +0700

    fix conflig file

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,109 @@
 <template>
-  <v-layout>
-    <v-navigation-drawer
-      v-model="drawer"
-      permanent
-    >
-      <v-list :items="items">
-        <v-list-item 
-          v-for="item in items"
-          :key="item.title"
-          :prepend-icon="item.icon"
-          :title="item.title"
-          :value="item.value"
+  <v-layout class="h-100">
+    <v-app-bar color="primary" prominent>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon
+          color="white"
+          variant="text"
+          @click.stop="drawer = !drawer"
         >
+        </v-app-bar-nav-icon>
+      </template>
+      <!-- <v-app-bar-nav-icon variant="text" icon="mdi-menu"></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>ระบบ Typography</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn
+            v-bind="props"
+            icon
+            class="text-black bg-white mr-6"
+            size="small"
+          >
+            {{ userProfile?.username }}
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="logout">
+            <v-list-item-title>ออกจากระบบ</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer :model-value="drawer">
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="item.title"
+          :to="item.value"
+          :exact="item.value === '/'"
+        >
+          <template v-slot:prepend>
+            <v-icon>{{ item.icon }}</v-icon>
+          </template>
+
+          <v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar color="primary" flat>
-      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main class="d-flex align-center justify-center">
-      <v-container>
-        <slot></slot>
-      </v-container>
+    <v-main class="ma-5">
+      <router-view />
     </v-main>
-
   </v-layout>
 </template>
-
 <script setup lang="ts">
-  import { ref } from 'vue'
-
-  const items = [
-    {
-      title: 'User manangement',
-      value: '/user',
-      icon: 'mdi-folder'
-    },
-    {
-      title: 'Factory',
-      value: '/factory',
-      icon: 'mdi-account-multiple'
-    },
-  ]
-
-  const drawer = ref(false)
-
+import { TIER } from "@/composables/api/useUserApi";
+import useAuth from "@/composables/useAuth";
+import { contextPluginSymbol, type PluginInstance } from "@/plugins/context";
+const { userProfile, refresh } = inject<PluginInstance>(contextPluginSymbol)!;
+const auth = useAuth();
+const drawer = ref(true);
+interface MenuItem {
+  title: string;
+  value: string;
+  icon: string;
+  tier?: TIER;
+}
+const logout = () => {
+  auth.logout();
+};
+const items = ref<MenuItem[]>([
+  {
+    title: "รายการสั่งผลิต",
+    value: "/",
+    icon: "mdi-folder",
+  },
+  {
+    title: "แดชบอร์ด",
+    value: "/dashboard",
+    icon: "mdi-collage",
+  },
+  {
+    title: "จัดการ User",
+    value: "/user",
+    icon: "mdi-account-multiple",
+    tier: TIER.ADMIN,
+  },
+  {
+    title: "จัดการโรงเรียน",
+    value: "/school",
+    icon: "mdi-school",
+    tier: TIER.ADMIN,
+  },
+  {
+    title: "ใบเสนอราคา",
+    value: "/quotation",
+    icon: "mdi-invoice",
+    tier: TIER.ADMIN,
+  },
+]);
+onMounted(async () => {
+  await refresh();
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,15 @@
 <template>
-  <v-card>
-    <template v-slot:title>
-      <h3>Hello world</h3>
-    </template>
-
-    <v-card-text class="pt-4">
-      <div v-if="!users || users.length === 0">No data</div>
-      <div v-else>
-        <v-table>
-          <thead>
-            <tr>
-              <th class="text-left">
-                ID
-              </th>
-              <th class="text-left">
-                Username
-              </th>
-              <th class="text-left">
-                Store
-              </th>
-              <th class="text-left">
-                Created Date
-              </th>
-              <th class="text-left">
-                Updated Date
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="user in users"
-              :key="user.id"
-            >
-              <td>{{ user.id }}</td>
-              <td>{{ user.username }}</td>
-              <td>{{ user.store }}</td>
-              <td>{{ useFormatDate(user.createdAt) }}</td>
-              <td>{{ useFormatDate(user.updatedAt) }}</td>
-            </tr>
-          </tbody>
-        </v-table>
-      </div>
-    </v-card-text>
-  </v-card>
+  <main>
+    <h1>รายการสั่งผลิต</h1>
+    <section class="pa-8">
+      <List />
+    </section>
+    <section>
+      <Table />
+    </section>
+  </main>
 </template>
-
 <script setup lang="ts">
-  import { useUserStore } from '@/stores/user';
-  
-  // ref
-  const store = useUserStore();
-  const { users } = storeToRefs(store);
-
-  onMounted(() => {
-    store.fetchUsers()
-  })
-
+import List from "@/components/production/list.vue";
+import Table from "@/components/production/tableStatus.vue";
 </script>
