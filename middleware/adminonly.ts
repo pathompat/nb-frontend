8f86436de48f23ee598a47ebf36c9903cdd52e7b
atippaz <nasakun13201@gmail.com>
2024-12-15 00:42:49 +0700

    remove toast

--- middleware/adminonly.ts
+++ middleware/adminonly.ts
@@ -3,7 +3,6 @@ import { toastPluginSymbol } from '~/plugins/toast'
 
 export default defineNuxtRouteMiddleware((to, from) => {
     const user = useAuthStore()
-    const toast = inject(toastPluginSymbol)!
     const { userProfile } = storeToRefs(user)
     if (userProfile.value!.role !== SYSTEM_ROLE.ADMIN) {
         return navigateTo('/')
